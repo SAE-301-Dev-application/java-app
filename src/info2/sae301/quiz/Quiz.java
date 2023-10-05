547061828d30675913cb1039bc17c57ec5a54996
Loïc Fgrs <faugieres.lolilolo@gmail.com>
2023-10-05 10:12:00 +0200

    Ajout des fichiers initiaux (src, gitignore, preferences Eclips

---
+++ src/info2/sae301/quiz/Quiz.java
@@ -0,0 +1,19 @@
+/*
+ * Quiz.java									                     06 oct.2023
+ * IUT de Rodez, pas de copyright ni de "copyleft".
+ */
+
+package info2.sae301.quiz;
+
+/**
+ * Application JavaFX de quiz pour les étudiants.
+ * 
+ * @author Loïc Faugières
+ * @author Simon Guiraud
+ * @author Florien Fabre
+ * @author Samuel Lacam
+ * @author Jonathan Guild
+ */
+public class Quiz {
+
+}
