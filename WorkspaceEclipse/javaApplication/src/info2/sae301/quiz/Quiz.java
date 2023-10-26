8eeada04efe810b2e64385d7c98cdabd78997beb
belicfr <pastels.vidange_Or@icloud.com>
2023-10-26 10:54:32 +0200

    Début MenuPrincipal.FXML

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Quiz.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Quiz.java
@@ -5,6 +5,15 @@
 
 package info2.sae301.quiz;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
 /**
  * Application JavaFX de quiz pour les étudiants.
  * 
@@ -14,6 +23,19 @@ package info2.sae301.quiz;
  * @author Samuel Lacam
  * @author Jonathan Guild
  */
-public class Quiz {
-
+public class Quiz extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("vues/MenuPrincipal.fxml"));
+			
+			primaryStage.show();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
