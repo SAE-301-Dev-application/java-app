eee9b92d106a5f8019496cc5ff161a502c6dbba7
Odonata971 <florian.fabre@iut-rodez.Fr>
2023-10-31 22:30:07 +0100

    Correction classe questions addition setters

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Categorie.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Categorie.java
@@ -62,5 +62,4 @@ public class Categorie {
     	}
     	return listeQuestions.isEmpty();
     }
-
 }
--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Question.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Question.java
@@ -54,27 +54,75 @@ public class Question {
 		this.categorie = categorie;
 	}
 
+
+	/** @return the intitule */
 	public String getIntitule() {
 		return intitule;
 	}
 
+
+	/** @return the reponseJuste */
 	public String getReponseJuste() {
 		return reponseJuste;
 	}
 
+
+	/** @return the reponsesFausses */
 	public String[] getReponsesFausses() {
 		return reponsesFausses;
 	}
 
+
+	/** @return the difficulte */
 	public int getDifficulte() {
 		return difficulte;
 	}
 
+
+	/** @return the feedback */
 	public String getFeedback() {
 		return feedback;
 	}
 
+
+	/** @return the categorie */
 	public Categorie getCategorie() {
 		return categorie;
+	}
+
+
+	/** @param intitule the intitule to set */
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+
+	/** @param reponseJuste the reponseJuste to set */
+	public void setReponseJuste(String reponseJuste) {
+		this.reponseJuste = reponseJuste;
+	}
+
+
+	/** @param reponsesFausses the reponsesFausses to set */
+	public void setReponsesFausses(String[] reponsesFausses) {
+		this.reponsesFausses = reponsesFausses;
+	}
+
+
+	/** @param difficulte the difficulte to set */
+	public void setDifficulte(int difficulte) {
+		this.difficulte = difficulte;
+	}
+
+
+	/** @param feedback the feedback to set */
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+
+	/** @param categorie the categorie to set */
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
 	}
 }
