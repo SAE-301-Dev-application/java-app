6c7bc41cc9cfdcbf60123b1782543a8c9fb30028
Odonata971 <florian.fabre@iut-rodez.Fr>
2023-11-03 15:20:11 +0100

    Peaufinage des commentaires et ajout test dans constructeur questions pour taille tableau reponsesFausses

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Categorie.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Categorie.java
@@ -2,14 +2,22 @@ package info2.sae301.quiz;
 
 import java.util.ArrayList; 
 
+/**
+ * TODO comment types
+ * @author flori
+ */
 public class Categorie {
     
+	/** l'intitulé de la catégorie */
     private String intitule;
+    
+    /** contient toutes les questions de la catégorie */
     private ArrayList<Question> listeQuestions = new ArrayList<Question> (); 
     
     
 
     /**
+     *  Nouvelle catégorie de questions identifiée par son intitulé
 	 * @param intitule
 	 * @param questions
 	 */
@@ -19,43 +27,65 @@ public class Categorie {
 	}
 
 
+	/** @return l'intitulé de la catégorie */
 	public String getIntitule() {
         return intitule;
     }
 
- 
-    public boolean ajouterQuestion(final Question aAjouter) {
+	
+    /**
+     * Ajoute à listeQuestions la question en paramètre
+     * @param aAjouter
+     * @return true si la question est ajoutée, false sinon
+     */
+    public boolean ajouterQuestion(Question aAjouter) {
     	return listeQuestions.add(aAjouter);
     	
     }
 
 
+    /** @return true si listeQuestions est vide, false sinon */
     public boolean estVide() {
         return listeQuestions.isEmpty();
     }
 
 
+    /**
+     * @return le nombre de questions présentes dans listeQuestions
+     */
     public int nbQuestions() {
         return listeQuestions.size();
     }
 
 
+    /**
+     * @return la liste des questions de la catégorie
+     */
     public ArrayList<Question> getListeQuestions() {
         return listeQuestions;
     }
 
 
-    /** @param intitule the intitule to set */
+    /** @param intitule l'intitule à changer */
 	public void setIntitule(String intitule) {
 		this.intitule = intitule;
 	}
 
 
-	public boolean supprimerQuestion(final Question aSupprimer) {
+	/**
+	 * Supprime de listeQuestions la question en paramètre
+	 * @param aSupprimer la question à supprimer de la catégorie
+	 * @return true si enlèvement réussi, false sinon
+	 */
+	public boolean supprimerQuestion(Question aSupprimer) {
 		return listeQuestions.remove(aSupprimer);
     }
 
 
+    /**
+     * Supprime toutes les questions de listeQuestions
+     * @return true si questions enlevées, false sinon
+     */
     public boolean supprimerToutesQuestions() {
     	while (!listeQuestions.isEmpty()) {
     		listeQuestions.remove(listeQuestions.get(0));
--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Question.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/Question.java
@@ -1,22 +1,33 @@
-
-
 package info2.sae301.quiz;
-
-import java.util.ArrayList;
 
 public class Question {
 	
-
+	/** l'intitulé de la question */
     private String intitule;
+    
+    /** la réponse juste */
     private String reponseJuste;
+    
+    /** les réponses fausses */
     private String[] reponsesFausses = new String[4];
+    
+    /** la difficulté de la question (1 : facile, 2 : moyen, 3 : difficile */
     private int difficulte;
+    
+    /** 
+     * le feedback à afficher lors de la correction 
+     * de la réponse à cette question
+     */
     private String feedback;
+    
+    /** la catégorie dans laquelle est cette question */
     private Categorie categorie;
 
     
     /**
-	 * Question qui ne contiendra pas de feedback
+     * Nouvelle question identifiée par son intitulé ayant une
+	 * réponse juste, des réponses fausses, un niveau de difficulté 
+	 * et une catégorie associée
 	 * @param intitule
 	 * @param reponseJuste
 	 * @param reponsesFausses
@@ -27,14 +38,21 @@ public class Question {
 			String[] reponsesFausses, int difficulte, Categorie categorie) {
 		this.intitule = intitule;
 		this.reponseJuste = reponseJuste;
-		this.reponsesFausses = reponsesFausses;
+		if (reponsesFausses.length > 4) {
+			throw new IllegalArgumentException("Le nombre max de réponses fausses est 4");
+		} else {
+			this.reponsesFausses = reponsesFausses;
+		}
 		this.difficulte = difficulte;
 		this.categorie = categorie;
+		this.feedback = null;
 	}
     
     
 	/**
-	 * Question contenant cette fois-ci un feedback
+	 * Nouvelle question identifiée par son intitulé ayant
+	 * une réponse juste, des réponses fausses, un niveau de difficulté,
+     * un feedback et une catégorie associée
 	 * @param intitule
 	 * @param reponseJuste
 	 * @param reponsesFausses
@@ -55,73 +73,73 @@ public class Question {
 	}
 
 
-	/** @return the intitule */
+	/** @return l'intitule de la question */
 	public String getIntitule() {
 		return intitule;
 	}
 
 
-	/** @return the reponseJuste */
+	/** @return la reponseJuste */
 	public String getReponseJuste() {
 		return reponseJuste;
 	}
 
 
-	/** @return the reponsesFausses */
+	/** @return les reponsesFausses */
 	public String[] getReponsesFausses() {
 		return reponsesFausses;
 	}
 
 
-	/** @return the difficulte */
+	/** @return la difficulte */
 	public int getDifficulte() {
 		return difficulte;
 	}
 
 
-	/** @return the feedback */
+	/** @return le feedback s'il existe */
 	public String getFeedback() {
 		return feedback;
 	}
 
 
-	/** @return the categorie */
+	/** @return la categorie */
 	public Categorie getCategorie() {
 		return categorie;
 	}
 
 
-	/** @param intitule the intitule to set */
+	/** @param intitule the intitule à changer */
 	public void setIntitule(String intitule) {
 		this.intitule = intitule;
 	}
 
 
-	/** @param reponseJuste the reponseJuste to set */
+	/** @param reponseJuste the reponseJuste à changer */
 	public void setReponseJuste(String reponseJuste) {
 		this.reponseJuste = reponseJuste;
 	}
 
 
-	/** @param reponsesFausses the reponsesFausses to set */
+	/** @param reponsesFausses the reponsesFausses à changer */
 	public void setReponsesFausses(String[] reponsesFausses) {
 		this.reponsesFausses = reponsesFausses;
 	}
 
 
-	/** @param difficulte the difficulte to set */
+	/** @param difficulte the difficulte à changer */
 	public void setDifficulte(int difficulte) {
 		this.difficulte = difficulte;
 	}
 
 
-	/** @param feedback the feedback to set */
+	/** @param feedback the feedback à changer */
 	public void setFeedback(String feedback) {
 		this.feedback = feedback;
 	}
 
 
-	/** @param categorie the categorie to set */
+	/** @param categorie the categorie à changer */
 	public void setCategorie(Categorie categorie) {
 		this.categorie = categorie;
 	}
