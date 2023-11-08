5c3ec1e8df6520e2ac0e3f67b1b751e87c7c20ed
Odonata971 <florian.fabre@iut-rodez.Fr>
2023-11-08 16:24:10 +0100

    Création classe sérialisation et debug création question margin aléatoire et appel d'un fichier css introuvable

---
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/serialisation/Serialisation.java
@@ -0,0 +1,13 @@
+/**
+ * Serialisation.java									8 nov. 2023 
+ * IUT de Rodez, no copyright ni "copyleft"
+ */
+package info2.sae301.quiz.serialisation;
+
+/**
+ * Classe de sérialisation pour les objets de type "Jeu" 
+ * @author FABRE Florian
+ */
+public class Serialisation {
+
+}
