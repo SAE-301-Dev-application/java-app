18f742236481c25b1eeba94e6df447068fefac07
Loïc Faugières <loic.faugieres@gmail.com>
2023-11-14 14:49:44 +0100

    Toutes les méthodes de ParametresPartie et début tests

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/modeles/ParametresPartie.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/modeles/ParametresPartie.java
@@ -8,7 +8,10 @@ package info2.sae301.quiz.modeles;
 import java.util.ArrayList;
 
 /**
- * TODO javadoc
+ * Les paramètres d'une partie de jeu.
+ * - Difficulté des questions
+ * - Nombre de questions
+ * - Catégories des questions
  * 
  * @author Florian Fabre
  * @author Loïc Faugières
@@ -21,8 +24,131 @@ public class ParametresPartie {
 	/** TODO javadoc */
 	private ArrayList<Categorie> categoriesSelectionnees;
 	
-	/** TODO javadoc */
+	/**
+	 * La difficulté des questions à proposer à l'utilisateur :
+	 * <ul>
+	 *     <li>- 0 : Indifférente</li>
+	 *     <li>- 1 : Facile</li>
+	 *     <li>- 2 : Moyenne</li>
+     *     <li>- 3: Difficile</li>
+     * </ul>
+	 */
+	private int difficulteQuestions;
+	
+	/**
+	 * Le nombre de questions à proposer à l'utilisateur :
+	 * <ul>
+	 *     <li>- 5</li>
+	 *     <li>- 10</li>
+	 *     <li>- 20</li>
+     * </ul>
+	 */
+	private int nombreQuestions;
+	
+	/**
+	 * Paramètres d'une partie de jeu nouvellement initialisée avec une difficulté
+	 * indifférente et 10 questions affichées dans le jeu par défaut.
+	 */
 	public ParametresPartie() {
 		this.categoriesSelectionnees = new ArrayList<Categorie>();
+		this.difficulteQuestions = 0; // Indifférente
+		this.nombreQuestions = 10;
 	}
+	
+	/**
+	 * Ajoute les catégories en paramètre à la liste des catégories
+	 * sélectionnées.
+	 * 
+	 * @param aSelectionner La liste des catégories à sélectionner.
+	 */
+	public void selectionnerCategories(ArrayList<Categorie> aSelectionner) {
+		this.categoriesSelectionnees.addAll(aSelectionner);
+	}
+	
+	/**
+	 * Vérification du fait qu'il y ait assez de questions dont la difficulté est
+	 * difficulteQuestions dans les catégories sélectionnées afin
+	 * d'afficher nombreQuestions questions.
+	 * 
+	 * @throws IllegalArgumentException si aucune question ne correspond aux critères.
+	 * @throws NumberFormatException si moins de questions que le nombre de questions
+	 * souhaitées correspondent aux critères.
+	 * @return true si il y a assez de questions.
+	 */
+	public void aAssezQuestions()
+	throws IllegalArgumentException, NumberFormatException {
+		final int NOMBRE_QUESTIONS
+		= choisirQuestionsProposees().size();
+		
+		final String AUCUNE_QUESTION
+		= "Il n'y a aucune question dans les catégories sélectionnées dont"
+		  + "la difficulté est égale à " + this.difficulteQuestions + ".\n"
+		  + "Veuillez entrer d'autres paramètres ou créer des questions.";
+		
+		final String MOINS_QUESTIONS
+		= "Seulement " + NOMBRE_QUESTIONS + " questions correspondent à vos "
+		  + "critères. Souhaitez-vous tout de même jouer ?";
+		
+		if (NOMBRE_QUESTIONS == 0) {
+			throw new IllegalArgumentException(AUCUNE_QUESTION);
+		}
+		if (this.nombreQuestions > NOMBRE_QUESTIONS) {
+			throw new NumberFormatException(MOINS_QUESTIONS);
+		}
+	}
+	
+	/**
+	 * Choisis en fonction des paramètres de la partie courante des questions
+	 * à proposer à l'utilisateur parmis les catégories sélectionnées.
+	 * 
+	 * @return La liste des questions correspondantes aux paramètres.
+	 */
+	public ArrayList<Question> choisirQuestionsProposees() {
+		ArrayList<Question> questions = new ArrayList<Question>();
+		
+		for (Categorie categorie : this.categoriesSelectionnees) {
+			if (difficulteQuestions == 0) {
+				questions.addAll(categorie.getListeQuestions());
+			} else {
+				// Recherche des questions dont la difficulté est en paramètre.
+				for (Question question : categorie.getListeQuestions()) {
+					if (question.getDifficulte() == this.getDifficulteQuestions()) {
+						questions.add(question);
+					}
+				}
+		    }
+		}
+		return questions;
+	}
+
+	/** @return Les catégories sélectionnées. */
+	public ArrayList<Categorie> getCategoriesSelectionnees() {
+		return categoriesSelectionnees;
+	}
+
+	/** @param categoriesSelectionnees Les catégories de questions sélectionnées. */
+	public void setCategoriesSelectionnees(ArrayList<Categorie> categoriesSelectionnees) {
+		this.categoriesSelectionnees = categoriesSelectionnees;
+	}
+
+	/** @return La difficulté des questions paramétrée. */
+	public int getDifficulteQuestions() {
+		return difficulteQuestions;
+	}
+
+	/** @param difficulteQuestions La difficulté des questions à proposer. */
+	public void setDifficulteQuestions(int difficulteQuestions) {
+		this.difficulteQuestions = difficulteQuestions;
+	}
+
+	/** @return Le nombre de questions paramétré. */
+	public int getNombreQuestions() {
+		return nombreQuestions;
+	}
+
+	/** @param nombreQuestions Le nombre de questions à proposer. */
+	public void setNombreQuestions(int nombreQuestions) {
+		this.nombreQuestions = nombreQuestions;
+	}
+	
 }
---
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/modeles/tests/TestParametresPartie.java
@@ -0,0 +1,130 @@
+/*
+ * TestParametresPartie.java             							14 nov. 2023
+ * IUT de Rodez, pas de copyright ni de "copyleft".
+ */
+
+package info2.sae301.quiz.modeles.tests;
+
+import info2.sae301.quiz.modeles.Categorie;
+import info2.sae301.quiz.modeles.Question;
+import info2.sae301.quiz.modeles.Jeu;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests unitaires de la classe {@link info2.sae301.quiz.modeles.ParametresPartie}.
+ * 
+ * @author Florian Fabre
+ * @author Loïc Faugières
+ * @author Jonathan Guil
+ * @author Simon Guiraud
+ * @author Samuel Lacam
+ */
+class TestParametresPartie {
+	
+	@BeforeEach
+	void init() {
+		
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#ParametresPartie()}.
+	 */
+	@Test
+	public void testConstructeur() {
+	    // TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#selectionnerCategories(ArrayList)}.
+	 */
+	@Test
+	public void testSelectionnerCategories() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#aAssezQuestions()}.
+	 */
+	@Test
+	public void testAAssezQuestions() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#choisirQuestionsProposees()}.
+	 */
+	@Test
+	public void testChoisirQuestionsProposees() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#getCategoriesSelectionnees()}.
+	 */
+	@Test
+	public void testGetCategoriesSelectionnees() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#setCategoriesSelectionnees(ArrayList)}.
+	 */
+	@Test
+	public void testSetCategoriesSelectionnees() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#getDifficulteQuestions()}.
+	 */
+	@Test
+	public void testGetDifficulteQuestions() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#setDifficulteQuestions(int)}.
+	 */
+	@Test
+	public void testSetDifficulteQuestions() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#getNombreQuestions()}.
+	 */
+	@Test
+	public void testGetNombreQuestions() {
+		// TODO le test
+	}
+	
+	/**
+	 * Méthode de test de la méthode
+	 * {@link info2.sae301.quiz.modeles.ParametresPartie#setNombreQuestions(int)}.
+	 */
+	@Test
+	public void testSetNombreQuestions() {
+		// TODO le test
+	}
+	
+}
