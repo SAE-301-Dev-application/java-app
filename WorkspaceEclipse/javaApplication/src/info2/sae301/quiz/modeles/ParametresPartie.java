174cfe6875b17b2a5db5e62d30b0a0ad3196d5a8
Loïc Faugières <loic.faugieres@gmail.com>
2023-11-15 13:31:13 +0100

    Adaptation des tests et du modèle ParametresPartie aux exceptions créées

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/modeles/ParametresPartie.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/modeles/ParametresPartie.java
@@ -5,6 +5,9 @@
 
 package info2.sae301.quiz.modeles;
 
+import info2.sae301.quiz.exceptions.NbInsuffisantQuestionsException;
+import info2.sae301.quiz.exceptions.AucuneQuestionCorrespondanteException;
+
 import java.util.ArrayList;
 
 /**
@@ -90,10 +93,10 @@ public class ParametresPartie {
 		  + "critères. Souhaitez-vous tout de même jouer ?";
 		
 		if (NOMBRE_QUESTIONS == 0) {
-			throw new IllegalArgumentException(AUCUNE_QUESTION);
+			throw new AucuneQuestionCorrespondanteException(AUCUNE_QUESTION);
 		}
 		if (this.nombreQuestions > NOMBRE_QUESTIONS) {
-			throw new NumberFormatException(MOINS_QUESTIONS);
+			throw new NbInsuffisantQuestionsException(MOINS_QUESTIONS);
 		}
 	}
 	
