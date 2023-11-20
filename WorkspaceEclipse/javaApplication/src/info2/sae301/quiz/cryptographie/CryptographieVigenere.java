d96b7fc3eca52951cd0b34c2590222f1f83401a4
Odonata971 <florian.fabre@iut-rodez.Fr>
2023-11-20 15:17:09 +0100

    Creation d'une methode trouverLettre pour trouver l'indice d'une lettre

--- WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/cryptographie/CryptographieVigenere.java
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/cryptographie/CryptographieVigenere.java
@@ -14,5 +14,34 @@ package info2.sae301.quiz.cryptographie;
  * @author Samuel Lacam
  */
 public class CryptographieVigenere {
-
+	
+	final String[] alphabet = {};
+	
+    /**
+     * Méthode de recherche linéaire afin de trouver l'indice 
+     * d'un élément dans un tableau
+     * @param alphabet 	tableau sur lequel on cherche l'indice
+     * 				   	d'un élément
+     * @param lettre 	lettre à qui on cherche l'indice
+     * @return 
+     */
+    public static int trouverLettre(int alphabet[], int lettre) { 
+       
+    	if (alphabet == null) { 
+            return -1; 
+        }
+    	
+    	int indice = -1;
+        int longueur = alphabet.length; 
+        
+        for (int i = 0; i < longueur; i++) {
+            if (alphabet[i] == lettre) { 
+                indice = i; 
+            } else { 
+                i = i + 1; 
+            } 
+        }
+        return indice; 
+    } 
+	
 }
