24801cde662bd0f9ef5034cacdba5634910429e6
Odonata971 <florian.fabre@iut-rodez.Fr>
2023-11-23 11:12:12 +0100

    Création d'un client test

---
+++ WorkspaceEclipse/javaApplication/src/info2/sae301/quiz/reseau/client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class client {
+    public static void main(String[] args) {
+        String serverAddress = "127.0.0.1"; // Change this to the server's address if not running locally
+        int serverPort = 65432;
+        
+        try {
+            Socket socket = new Socket(serverAddress, serverPort);
+            
+            // Create input and output streams for the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            // Read input from the user and send it to the server
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            String message;
+            while ((message = userInput.readLine()) != null) {
+                out.println(message);
+                String response = in.readLine();
+                System.out.println("Server response: " + response);
+            }
+            
+            // Close the client socket
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
